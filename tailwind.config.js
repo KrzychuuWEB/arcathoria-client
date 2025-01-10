8f530385ccc26d0d2883488e913c1a8d199496f0
Krzysztof <krzychuuweb@gmail.com>
2025-01-10 16:30:34 +0100

    Add default background and create login form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'arcathoria-default': "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('/src/assets/background.png')",
+            },
+        },
+    },
+    plugins: [],
 }
 
