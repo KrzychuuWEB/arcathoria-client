49cbad40e22b1442c7ae392aa1e964b449a29ec2
Krzysztof <krzychuuweb@gmail.com>
2025-03-02 21:32:37 +0100

    Created form for create character and validation. Moved StatusBar from components to features/character/components

--- src/features/character/pages/CreateCharacterPage.jsx
+++ src/features/character/pages/CreateCharacterPage.jsx
@@ -1,5 +1,15 @@
+import LandingLayout from "../../../layouts/LandingLayout.jsx";
+import CreateCharacterForm from "../forms/CreateCharacterForm.jsx";
+import AuthContainer from "../../account/components/AuthContainer.jsx";
+
 const CharacterPage = () => {
-    return <div></div>;
+    return (
+        <LandingLayout>
+            <AuthContainer title="Stwórz nową postać">
+                <CreateCharacterForm />
+            </AuthContainer>
+        </LandingLayout>
+    );
 };
 
 export default CharacterPage;
