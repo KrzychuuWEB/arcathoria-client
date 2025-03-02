ff4748c34834fb1df1b2b072a305ca076842e635
Krzysztof <krzychuuweb@gmail.com>
2025-03-02 11:09:21 +0100

    Added ProtectedRoute for public and private route with auth required

--- src/routes/GameRoutes.jsx
+++ src/routes/GameRoutes.jsx
@@ -1,11 +1,14 @@
 import {Route} from "react-router-dom";
 import {paths} from "./paths.js";
 import DashboardPage from "../features/dashboard/pages/DashboardPage.jsx";
+import ProtectedRoute from "./ProtectedRoute.jsx";
 
 const GameRoutes = () => {
     return (
         <>
-            <Route path={paths.game.dashboard} element={<DashboardPage/>}/>
+            <Route element={<ProtectedRoute requiresAuth={true}/>}>
+                <Route path={paths.game.dashboard} element={<DashboardPage/>}/>
+            </Route>
         </>
     );
 };
---
+++ src/routes/ProtectedRoute.jsx
@@ -0,0 +1,24 @@
+import {Navigate, Outlet} from "react-router-dom";
+import PropTypes from "prop-types";
+import useAuth from "../hooks/useAuth.jsx";
+import {paths} from "./paths.js";
+
+const ProtectedRoute = ({requiresAuth}) => {
+    const {isAuthenticated} = useAuth();
+
+    if (requiresAuth && !isAuthenticated()) {
+        return <Navigate to={paths.auth.login}/>;
+    }
+
+    if (!requiresAuth && isAuthenticated()) {
+        return <Navigate to={paths.game.dashboard}/>;
+    }
+
+    return <Outlet/>;
+};
+
+ProtectedRoute.propTypes = {
+    requiresAuth: PropTypes.bool.isRequired,
+};
+
+export default ProtectedRoute;
