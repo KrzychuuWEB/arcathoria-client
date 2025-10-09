f0c8d5e1223d5e3c20741777d8a831c8a83b4183
Krzysztof <krzychuuweb@gmail.com>
2025-10-09 13:44:48 +0200

    feat(migrate): update tailwind global.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 const App = () => {
-    return (
-        <div>
-            <div className="h-16 w-16 bg-red-500 rounded-md shadow-lg" />
-            <div className="h-16 w-16 bg-primary-300 rounded-md shadow-lg" />
-        </div>
-    );
+    return <div></div>;
 };
 
 export default App;
