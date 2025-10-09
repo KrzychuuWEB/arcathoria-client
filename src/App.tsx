bcadd7afa5a8662bb2e0c4d76fcad36a32301102
Krzysztof <krzychuuweb@gmail.com>
2025-10-09 12:46:03 +0200

    feat(migrate): delete all plain javascript and add typescript config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <div>
+            <div className="h-16 w-16 bg-red-500 rounded-md shadow-lg" />
+            <div className="h-16 w-16 bg-primary-300 rounded-md shadow-lg" />
+        </div>
+    );
+};
+
+export default App;
