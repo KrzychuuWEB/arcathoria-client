37ca071b83a34b0b2c0ccbf50c0c2349880bb76b
Krzysztof <krzychuuweb@gmail.com>
2025-10-29 16:53:16 +0100

    feat(api): enhance error strategies and simplify `bootstrapLoader`
    - Updated `errorCodeStrategies` to use more informative and user-friendly error handling.
    - Replaced `IgnoreStrategy` with contextual strategies like `FormStrategy`, `RedirectStrategy`, and `ToastStrategy`.
    - Simplified `bootstrapLoader` by directly returning the session object.

--- src/app/guard/bootstrapLoader.ts
+++ src/app/guard/bootstrapLoader.ts
@@ -1,12 +1,11 @@
 import { ensureSessionSummary } from "@app/guard/query.ts";
 
 export const bootstrapLoader = () => async () => {
-    const session = await ensureSessionSummary().catch(() => ({
+    return await ensureSessionSummary().catch(() => ({
         isAuthenticated: false,
         hasCharacter: false,
         inBattle: false,
         userId: null,
         characterId: null,
     }));
-    return session;
 };
