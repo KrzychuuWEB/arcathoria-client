08bfa0fc407ff13906796a8616e11784b19a1912
Krzysztof <krzychuuweb@gmail.com>
2025-10-30 17:14:52 +0100

    refactor(api): remove `ensureAccount` export from account query hook

--- src/api/queries/account/queries.ts
+++ src/api/queries/account/queries.ts
@@ -2,6 +2,6 @@ import { makeQuery } from "@api/queries/makeQuery.ts";
 import { getMyAccountQueryOptions } from "@api/orval.ts";
 import { mapAccountDTO } from "@domain/account/mapper.ts";
 
-export const [useAccount, ensureAccount] = makeQuery(() =>
+export const [useAccount] = makeQuery(() =>
     getMyAccountQueryOptions({ query: { select: mapAccountDTO } }),
 );
